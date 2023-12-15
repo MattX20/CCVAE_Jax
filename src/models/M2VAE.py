c75d6c47340b9ebdd3c01ad0deed20122ee3973c
MattX20 <matt.dinot@gmail.com>
2023-12-15 23:08:30 +0100

    tried to use numpyro with M2

--- src/models/M2VAE.py
+++ src/models/M2VAE.py
@@ -1,83 +1,125 @@
-from typing import Type
+from typing import Type, Tuple
 
 import jax.numpy as jnp
 from jax import random
+
 from flax import linen as nn
 
+import numpyro
+from numpyro.contrib.module import flax_module
+import numpyro.distributions as dist
 
-class M2AboveLayers(nn.Module):
-    """
-        M2AboveLayers returns the mean and log variance (latent_dim vectors), and logits (num_classes vector)
-        from an input_dim vector.
-    """
-    input_dim: int
-    latent_dim: int
+
+class M2FirstEncoder(nn.Module):
+    encoder_class: Type[nn.Module]
     num_classes: int
-
-    @nn.compact
-    def __call__(self, x):
-        # Input x is expected to be of shape (batch_size, input_dim)
-        mu = nn.Dense(features=self.latent_dim)(x)
-        logvar = nn.Dense(features=self.latent_dim)(x)
-        logy = nn.Dense(features=self.num_classes)(x)
-        return mu, logvar, logy
-
-class M2BelowLayers(nn.Module):
-    """
-        M2BelowLayers returns a output_dim vector from a latent_dim vector.
-    """
     latent_dim: int
-    output_dim: int
 
-    @nn.compact
-    def __call__(self, x):
-        # Input x is expected to be of shape (batch_size, latent_dim)
-        x = nn.Dense(features=self.output_dim)(x)
-        return x
+    def setup(self):
+        self.encoder = self.encoder_class()
+        self.fc_h = nn.Dense(features=self.latent_dim)
+        self.fc_y = nn.Dense(features=self.num_classes)
     
-
-class M2VAE(nn.Module):
-    """
-        M2VAE is the implementation of the M2 model from 'Semi-supervised learning with deep generative models.' by Kingma et al.
-        Given an encoder class, decoder class, a latent dim and a number of classes, returns a similar shape reconstructed input,
-        mu a latent_dim vector (mean of latent), logvar a latent_dim vector (log variance of latent), and logy a num_classes vector
-        (logits for classification).
-    """
-    encoder: nn.Module
-    decoder: nn.Module
-    above_layers: nn.Module
-    below_layers: nn.Module
-    latent_dim: int
-    num_classes: int
-
-    def __call__(self, x, rng_key):
-        # Input x is expected to be of shape (batch_size, ...), and rng_key a jax random key
-
-        # Encoding
+    def __call__(self, x):
         x = self.encoder(x)
-        mu, logvar, logy = self.above_layers(x)
-
-        # Reparameterization trick
-        std = jnp.exp(0.5 * logvar)
-        epsilon = random.normal(rng_key, mu.shape)
-        z = mu + std * epsilon  
-
-        # Decoding
+        h = self.fc_h(x)
+        logy = self.fc_y(x)
+        h = nn.relu(h)
         y = nn.softmax(logy)
-        zy = jnp.concatenate([z, y], axis=-1)
-        zy = self.below_layers(zy)
+
+        return h, y
+
+class M2SecondEncoder(nn.Module):
+    latent_dim: int
+
+    @nn.compact
+    def __call__(self, h, y_one_hot):
+        hy = jnp.concatenate([h, y_one_hot], axis=-1)
+        loc = nn.Dense(features=self.latent_dim)(hy)
+        log_scale = nn.Dense(features=self.latent_dim)(hy)
+        scale = jnp.exp(log_scale)
+
+        return loc, scale
+
+class M2Decoder(nn.Module):
+    decoder_class: Type[nn.Module]
+
+    def setup(self):
+        self.decoder = self.decoder_class()
+        self.fc = nn.Dense(features=self.decoder.input_dim)
+    
+    def __call__(self, z, y_one_hot):
+        zy = jnp.concatenate([z, y_one_hot], axis=-1)
+        zy = self.fc(zy)
         reconstructed_x = self.decoder(zy)
+        return reconstructed_x
+
+
+class M2VAE:
+
+    def __init__(self,
+                 encoder_class: Type[nn.Module],
+                 decoder_class: Type[nn.Module],
+                 num_classes: int,
+                 latent_dim: int,
+                 img_shape: Tuple[int, int, int]):
         
-        return reconstructed_x, mu, logvar, logy
+        self.encoder_class = encoder_class
+        self.decoder_class = decoder_class
+        self.num_classes = num_classes
+        self.latent_dim = latent_dim
+        self.img_shape = img_shape
+    
+    def model(self, xs, ys=None):
+        batch_size = xs.shape[0]
 
+        decoder = flax_module(
+            "decoder", 
+            M2Decoder(self.decoder_class), 
+            z=jnp.ones((1, self.latent_dim)),
+            y_one_hot=jnp.ones((1, self.num_classes))
+        ) 
 
-def instanciate_MVAE(encoder_class: Type[nn.Module], decoder_class: Type[nn.Module], latent_dim: int, num_classes: int):
-    encoder = encoder_class()
-    decoder = decoder_class()
-    above_layers = M2AboveLayers(input_dim=encoder.output_dim, 
-                                            latent_dim=latent_dim, 
-                                            num_classes=num_classes)
-    below_layers = M2BelowLayers(latent_dim=latent_dim + num_classes,
-                                        output_dim=decoder.input_dim)
+        # with numpyro.plate("data", batch_size):
 
-    return M2VAE(encoder, decoder, above_layers, below_layers, latent_dim, num_classes)
+        prior_loc = jnp.zeros((batch_size, self.latent_dim))
+        prior_scale = jnp.ones((batch_size, self.latent_dim))
+        zs = numpyro.sample("z", dist.Normal(prior_loc, prior_scale))
+
+        alpha_prior = jnp.ones((batch_size, self.num_classes)) / self.num_classes
+        ys = numpyro.sample("y", dist.Categorical(alpha_prior), obs=ys)
+
+        if ys is not None:
+            with numpyro.handlers.scale(scale=100.):
+                numpyro.sample("y_aux", dist.Categorical(alpha_prior), obs=ys)
+        
+        y_one_hot = jnp.eye(self.num_classes)[ys]
+        loc = decoder(zs, y_one_hot)
+        numpyro.sample("x", dist.Bernoulli(loc), obs=xs)
+
+        return loc
+
+    def guide(self, xs, ys=None):
+        batch_size = xs.shape[0]
+
+        encoder1 = flax_module(
+            "encoder1",
+            M2FirstEncoder(self.encoder_class, self.num_classes, self.latent_dim),
+            x=jnp.ones((1,) + self.img_shape)
+        )
+
+        encoder2 = flax_module(
+            "encoder2",
+            M2SecondEncoder(self.latent_dim),
+            h=jnp.ones((1, self.latent_dim)),
+            y_one_hot=jnp.ones((1, self.num_classes))
+        )
+
+        # with numpyro.plate("data", batch_size):
+
+        h, y_prob = encoder1(xs)
+        if ys is None:
+            ys = numpyro.sample("y", dist.Categorical(y_prob))
+        y_one_hot = jnp.eye(self.num_classes)[ys]
+        loc, scale = encoder2(h, y_one_hot)
+        numpyro.sample("z", dist.Normal(loc, scale))
