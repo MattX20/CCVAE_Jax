cd8df89ea38bdf85dd61614c2fc70daedf6bdd1f
MattX20 <matt.dinot@gmail.com>
2023-12-18 11:07:04 +0100

    modified gitignore

---
+++ src/models/CCVAE.py
@@ -0,0 +1,10 @@
+from typing import Type, Tuple
+
+import jax.numpy as jnp
+from jax import random
+
+from flax import linen as nn
+
+import numpyro
+from numpyro.contrib.module import flax_module
+import numpyro.distributions as dist
